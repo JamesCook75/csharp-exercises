e79c8ef45310f5be41b8aee1b199a8b715c4d858
James Cook <jamescook.work@gmail.com>
2018-02-19 16:06:58 -0500

    Interim commit. Working on 'Restaurant Menu Continued'

--- Restaurant/Menu.cs
+++ Restaurant/Menu.cs
@@ -6,6 +6,34 @@ namespace Restaurant
 {
     class Menu
     {
-        private List<MenuItem> Items { get; set; }
+        private string Name { get; set; }
+        public List<MenuItem> Items { get; set; }
+
+        public Menu(string name, List<MenuItem> items)
+        {
+            Name = name;
+            Items = items;
+        }
+
+            public void AddItem(MenuItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(MenuItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public string PrintMenu()
+        {
+            string output = "";
+            foreach (MenuItem item in Items)
+            {
+                output += "Name: " + item.Name + "\nDescription: " + item.Description + "\nPrice: " + item.Price + "\n\n--------";
+            }
+
+            return output;
+        }
     }
 }
--- Restaurant/MenuItem.cs
+++ Restaurant/MenuItem.cs
@@ -6,17 +6,27 @@ namespace Restaurant
 {
     public class MenuItem
     {
-        private double Price { get; set; }
-        private string Description { get; set; }
-        private string Category { get; set; }
 
-        public MenuItem(double price, string description,
-                    string category)
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime Created { get; set; }
+
+        public MenuItem(string name, double price, string description,
+                    string category, DateTime created)
         {
+            Name = name;
             Price = price;
             Description = description;
             Category = category;
+            Created = created;
         }
+
+        public MenuItem(string name, double price)
+            : this(name, price, "", "", DateTime.Now) { }
+
+
     }
 }
 
